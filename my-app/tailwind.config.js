bac520fe00d65ba36daf7fafd9688ff5db29fedd
AmundAudum <Amundaudum@gmail.com>
2024-05-30 08:05:17 +0200

    fiksing

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors:{
+        bgblack: "#1C1C1C",
+        skyblue: "#57C1E9"
+      }
+    },
+  },
+  plugins: [],
+}
+
